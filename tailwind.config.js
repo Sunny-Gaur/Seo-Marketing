d331d091ee5d15b8264bfdfce7f067a6a02d3d78
Sunny <bobby149@gmail.com>
2024-05-09 13:40:24 +0530

    working project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["*.html", "*.js"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-image':'url("https://assets.nicepagecdn.com/11a8ddce/4222421/images/Untitled-3copy.jpg")',
+        'curve-image-bg':'url("https://assets.nicepagecdn.com/11a8ddce/4222421/images/45.jpg")',
+        'hand-img':'url(https://assets.nicepagecdn.com/11a8ddce/4222421/images/16691741_5799687.png)',
+        'Laptop':'url(https://assets.nicepagecdn.com/11a8ddce/4222421/images/17879734_5883631.png)',
+        'work-img':'url("https://assets.nicepagecdn.com/11a8ddce/4222421/images/Untitled-31.jpg")',
+        'circle-img':'url("https://assets.nicepagecdn.com/11a8ddce/4222421/images/young-beautiful-woman-using-mobile-phone-studio-pink-color-studio-background-human-facial-emotions-concept-trendy-colors.jpg")',
+        'case-study':'url("https://assets.nicepagecdn.com/11a8ddce/4222421/images/Untitled-31.jpg")',
+        'touch':'url("https://assets.nicepagecdn.com/11a8ddce/4222421/images/45.jpg")',
+      },
+      colors:{
+        'btn-color':"#F1C50E",
+        'card-color':"#FFE7FD",
+        'card-btn':"#FC10E7",
+        'card-circle':"#F98813",
+        'mypink':"#FFF4F0",
+      }
+    },
+  },
+  plugins: [],
+}
